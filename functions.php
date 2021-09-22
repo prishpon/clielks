a399f67f82739d770925ffa83c2a6506d2374c4d
prishpon <pryshlyakolga09@gmail.com>
2021-09-22 16:28:10 +0300

    header

--- functions.php
+++ functions.php
@@ -11,5 +11,112 @@ function theme_enqueue_scripts() {
 	}
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 
+//
+add_action('init', 'service_custom_post_type');
+function service_custom_post_type(){
 
+	register_post_type('service', array(
+		'labels'             => array(
+			'name'               => 'service', 
+			'singular_name'      => 'service', 
+			'add_new'            => 'Add new',
+			'add_new_item'       => 'Add new service',
+			'edit_item'          => 'Edit service ',
+			'new_item'           => 'New service',
+			'view_item'          => 'View service',
+			'search_items'       => 'Search',
+			'not_found'          =>  'not found',
+			'not_found_in_trash' => 'not found in trash',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Services'
+
+		  ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'menu_position'      => 16,
+        'menu_icon'          => 'dashicons-welcome-add-page',
+		'supports'           => array('title','editor','thumbnail','excerpt','custom-fields')
+	
+		) );
+
+}
+//gallery post type
+add_action('init', 'gallery_custom_post_type');
+function gallery_custom_post_type(){
+
+	register_post_type('gallery', array(
+		'labels'             => array(
+			'name'               => 'gallery', 
+			'singular_name'      => 'Gallery', 
+			'add_new'            => 'Add new',
+			'add_new_item'       => 'Add new gallery',
+			'edit_item'          => 'Edit gallery',
+			'new_item'           => 'New gallery',
+			'view_item'          => 'View gallery',
+			'search_items'       => 'Search gallery',
+			'not_found'          =>  'gallery not found',
+			'not_found_in_trash' => 'not found in trash',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Галерея'
+
+		  ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'menu_position'      => 15,
+        'menu_icon'          => 'dashicons-welcome-add-page',
+		'supports'           => array('title','editor','thumbnail')
+	
+		) );
+
+}
+//silcon post type
+add_action('init', 'silcon_custom_post_type');
+function silcon_custom_post_type(){
+
+	register_post_type('silcon', array(
+		'labels'             => array(
+			'name'               => 'silcon', 
+			'singular_name'      => 'Потолок', 
+			'add_new'            => 'Add new',
+			'add_new_item'       => 'Add new gallery',
+			'edit_item'          => 'Edit gallery',
+			'new_item'           => 'New gallery',
+			'view_item'          => 'View gallery',
+			'search_items'       => 'Search gallery',
+			'not_found'          =>  'gallery not found',
+			'not_found_in_trash' => 'not found in trash',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Виды потолков '
+
+		  ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'menu_position'      => 14,
+        'menu_icon'          => 'dashicons-welcome-add-page',
+		'supports'           => array('title','editor','thumbnail')
+	
+		) );
+
+}
 
--- header.php
+++ header.php
@@ -10,9 +10,68 @@
     <?php wp_head(); ?>
   </head>
 <body>
-  <section>
-      <header>
-        <div class="topheader container">
+  <section class="main-header">
+      <header class="container">
+        <div class="topheader row">
+           
+               <div class="top-item row">
+                  <div>
+                  <i class="fa fa-globe" aria-hidden="true"></i>
+                  </div>
+                  <div>
+                    <img src="<?php bloginfo("template_directory"); ?> /img/ru.png" alt="">
+                    <div class="top-sub-item">Rus</div>
+                  </div>
+                  <div>
+                  <img src="<?php bloginfo("template_directory"); ?>/img/uk.png" alt="">
+                    <div class="top-sub-item">Ukr</div>
+           
+                  </div>      
+               </div>
+               <div class="top-item row">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <div class="top-sub-item">
+                      Выбрать ваш город
+                       установки потолков
+                                  </div>
+               </div>
+               <div class="top-item row">
+                   <i class="fa fa-clock-o" aria-hidden="true"></i>
+                     <div class="top-sub-item">
+                            пн-вс 9:00-18:00
+                      </div>
+               </div>
+                <div class="top-item row">
+                   <img class="top-social" src="<?php bloginfo("template_directory"); ?>/img/Telegram_H.png" alt="">
+                    <img class="top-social" src="<?php bloginfo("template_directory"); ?>/img/viber_h.png" alt="">
+                    <img class="top-social" src="<?php bloginfo("template_directory"); ?>/img/whatsappS-1.png" alt="">
+                </div>
+                 <div class="top-item row">
+                   <div>
+                      <i class="fa fa-phone" aria-hidden="true"></i>
+                   </div>
+                      <div class="column">
+                         <div class="top-sub-item">
+                             +38 (067) 611 66 12
+                         </div>
+                         <div class="top-sub-item">
+                             +38 (067) 611 66 12
+                         </div>
+                      </div>
+                       <div class="column">
+                       <div class="top-sub-item">
+                             +38 (067) 611 66 12
+                         </div>
+                         <div class="top-sub-item">
+                             <button class="primary">
+                               Заказать звонок
+                             </button>
+                         </div>
+                       </div>
+                  </div>
+           </div>
+              
+ <div class="midheader container">
                      <nav>
                            <?php wp_nav_menu( [
 		                          'theme_location'  => 'primary',
